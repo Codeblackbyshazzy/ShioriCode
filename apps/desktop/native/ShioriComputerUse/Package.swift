63075586fd597048e022445c04e5f7a7451d2895
Sami Hindi <sami@samihindi.com>
2026-05-23 11:37:31 +0200

    fix(desktop): vendor Permiso helper source

--- apps/desktop/native/ShioriComputerUse/Package.swift
+++ apps/desktop/native/ShioriComputerUse/Package.swift
@@ -1,25 +1,20 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.0
 import PackageDescription
 
 let package = Package(
     name: "ShioriComputerUse",
     platforms: [
-        .macOS(.v26)
+        .macOS(.v15)
     ],
     products: [
         .executable(name: "ShioriComputerUseHelper", targets: ["ShioriComputerUseHelper"])
     ],
-    dependencies: [
-        .package(
-            url: "https://github.com/zats/permiso.git",
-            revision: "3012871b741f68b1b6f46e2e1936c422df703968"
-        )
-    ],
     targets: [
+        .target(name: "Permiso"),
         .executableTarget(
             name: "ShioriComputerUseHelper",
             dependencies: [
-                .product(name: "Permiso", package: "permiso")
+                "Permiso"
             ]
         )
     ]
